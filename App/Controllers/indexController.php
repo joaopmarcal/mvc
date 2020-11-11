ac4c8df7b0d226d47855b403fa6cc8e9d56d7717
João Paulo <joaopaulozangrande@gmail.com>
2020-11-11 20:45:04 -0300

    abstraction of the Controllers

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -2,13 +2,9 @@
 
   namespace App\Controllers;
 
-  class IndexController {
+  use MF\Controller\Action;
 
-    private $view;
-
-    public function __construct(){
-      $this->view = new \stdClass();
-    }
+  class IndexController extends Action {
 
     public function index(){
 
@@ -20,16 +16,6 @@
 
       $this->view->dados = array("Notebook", "Smartphone");
       $this->render('sobreNos');
-    }
-
-    public function render($view){
-      $classAtual = get_class($this);
-
-      $classAtual = str_replace('App\\Controllers\\','', $classAtual);
-
-      $classAtual = strtolower(str_replace('Controller','', $classAtual));
-
-      require_once "../App/Views/". $classAtual ."/". $view .".phtml";
     }
 
   }
