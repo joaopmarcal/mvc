db51dc713f66564100585664b7872a469c616a03
João Paulo <joaopaulozangrande@gmail.com>
2020-11-11 14:00:45 -0300

    routes

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
     require_once "../vendor/autoload.php";
 
+    $route = new \App\route;
     echo "Isso está funcionando!!";
+    echo "<hr>";
+    print_r($route->getUrl());
 
   /* php -S localhost:8081*/
 ?>
