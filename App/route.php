e8259ece9be6166524e0edae6c0fb0cb650d0bd3
João Paulo <joaopaulozangrande@gmail.com>
2020-11-11 17:03:58 -0300

    Abstraction of the Routes class

--- App/route.php
+++ App/route.php
@@ -2,32 +2,11 @@
 
     namespace App;
 
-    class route {
+    use MF\Init\Bootstrap;
 
-      private $routes;
+    class route extends Bootstrap {
 
-      public function __construct(){
-        $this->initRoutes();
-        $this->run($this->getUrl());
-      }
-
-      /**
-       * @return mixed
-       */
-      public function getRoutes()
-      {
-        return $this->routes;
-      }
-
-      /**
-       * @param mixed $routes
-       */
-      public function setRoutes(array $routes)
-      {
-        $this->routes = $routes;
-      }
-
-      public function initRoutes(){
+      protected function initRoutes(){
 
         $routes['home'] = array(
           'route' => '/',
@@ -43,25 +22,6 @@
 
         $this->setRoutes($routes);
 
-      }
-
-      public function run($url){
-        echo "--".$url."--";
-        foreach ($this->getRoutes() as $key => $route){
-          if ($url == $route['route']){
-            $class = "App\\Controllers\\" . ucfirst($route['controller']);
-
-            $controller = new $class;
-
-            $action = $route['action'];
-
-            $controller->$action();
-          }
-        }
-      }
-
-      public function getUrl(){
-        return parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
       }
 
     }
