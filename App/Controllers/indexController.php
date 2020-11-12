e9fa0221b40db7ae459f266a52b647c001ad790f
João Paulo <joaopaulozangrande@gmail.com>
2020-11-11 21:25:27 -0300

    reusing an layout

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -9,13 +9,13 @@
     public function index(){
 
       $this->view->dados = array("Sofá", "Cadeira", "Cama");
-      $this->render('index');
+      $this->render('index', 'layout1');
     }
 
     public function sobreNos(){
 
       $this->view->dados = array("Notebook", "Smartphone");
-      $this->render('sobreNos');
+      $this->render('sobreNos', 'layout2');
     }
 
   }
